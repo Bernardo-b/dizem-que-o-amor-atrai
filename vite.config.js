bff9f64fb70181f5e53910623f32f96ebf85bcf7
Bernardo Bacha <bernardo.bacha@ifood.com.br>
2026-03-12 20:21:28 -0300

    Corrigir base path para dizem-que-o-amor-atrai

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/surpanila/' : '/',
+  base: command === 'build' ? '/dizem-que-o-amor-atrai/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
